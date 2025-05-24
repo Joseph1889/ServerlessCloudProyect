306ea4882243c0f33eda1e42242167167518c50e
Joseph1889 <josephh2408gmail.com>
2025-05-23 21:47:21 -0500

    Update main.tf lambdas

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-data "archive_file_Get" "lambda_zip_Get" {
+data "archive_file" "lambda_zip_get" {
   type        = "zip"
   source_dir  = "${path.module}/lambdaGet"
   output_path = "${path.module}/lambda_function_Get.zip"
@@ -56,11 +56,11 @@ resource "aws_lambda_function_Get" "mi_lambda_Get" {
   handler       = "handler.handler"
   runtime       = "nodejs22.x"
 
-  filename         = data.archive_file_Get.lambda_zip_Get.output_path
-  source_code_hash = data.archive_file_Get.lambda_zip_Get.output_base64sha256
+  filename         = data.archive_file.lambda_zip_get.output_path
+  source_code_hash = data.archive_file.lambda_zip_get.output_base64sha256
 }
 
-data "archive_file_Post" "lambda_zip_Post" {
+data "archive_file" "lambda_zip_post" {
   type        = "zip"
   source_dir  = "${path.module}/lambdaPost"
   output_path = "${path.module}/lambda_function_Post.zip"
@@ -72,8 +72,8 @@ resource "aws_lambda_function_Post" "mi_lambda_get" {
   handler       = "handler.handler"
   runtime       = "nodejs22.x"
 
-  filename         = data.archive_file_Post.lambda_zip_Post.output_path
-  source_code_hash = data.archive_file_Post.lambda_zip_Post.output_base64sha256
+  filename         = data.archive_file.lambda_zip_post.output_path
+  source_code_hash = data.archive_file.lambda_zip_post.output_base64sha256
 }
 
 
