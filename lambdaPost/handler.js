7c6fed0adb85698249c9bd81ad323e3b5b5b8ed4
Joseph1889 <josephh2408gmail.com>
2025-05-24 22:26:22 -0500

    Update handler POST

--- lambdaPost/handler.js
+++ lambdaPost/handler.js
@@ -1,11 +1,40 @@
-exports.handler = async (event) => {
+const {DynamoDBClient}  = require("@aws-sdk/client-dynamodb");
+const { PutCommand, DynamoDBDocumentClient} = require("@aws-sdk/lib-dynamodb");
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ message: "Hola desde LambdaPost!" })
-  };
-};
+const client = new DynamoDBClient({});
+const ddb = DynamoDBDocumentClient.from(client);
+
+const TABLE_NAME = "MiTablaDynamoDB";
+
+exports.handler = async (event) =>  {
+  try {
+    console.log("EVENT:", JSON.stringify(event));
+
+    // Quita validación de método HTTP
+    const body = JSON.parse(event.body);
+
+    if (!body.ID_Dron) {
+      body.ID_Dron = Date.now().toString(); 
+    }
+
+    const command = new PutCommand({
+      TableName: TABLE_NAME,
+      Item: body,
+    });
+
+    await ddb.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Item saved successfully', item: body }),
+    };
+
+  } catch (error) {
+    console.error('Error saving item to DynamoDB:', error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error', error: error.message }),
+    };
+  }
+}
